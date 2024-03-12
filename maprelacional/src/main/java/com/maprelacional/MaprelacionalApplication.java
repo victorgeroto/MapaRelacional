e109528339f7e514274069c7c31f3d0185a22a1e
victorgeroto <132359789+victorgeroto@users.noreply.github.com>
2024-03-12 08:45:17 -0300

    Add files via upload

---
+++ maprelacional/src/main/java/com/maprelacional/MaprelacionalApplication.java
@@ -0,0 +1,13 @@
+package com.maprelacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaprelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaprelacionalApplication.class, args);
+	}
+
+}
